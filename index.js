dfff0a993942b17b94279d3197e67400a67c2d0a
Nguyenhuynh201 <nguyen.huynh01@student.csulb.edu>
2021-04-27 14:37:07 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+const path = require("path");
+
+app.listen(3000, () => { {
+  console.log("Server started (http://localhost:3000/) !");
+}});
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => { 
+  //res.send ("Hello world...");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/data", (req, res) => {
+  const test = {
+    title: "Test",
+    items: ["one", "two", "three"]
+  };
+  res.render("data", { model: test });
+});
